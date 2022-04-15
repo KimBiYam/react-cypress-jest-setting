665053583a16a14a33cf54d0d126c4a47a1b17e1
ChangHyunKim <hot9998@naver.com>
2022-04-15 23:19:03 +0900

    Add coverage settings

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = ({ env }) => {
+  const isEnvE2e = env === "e2e";
+
+  return {
+    ...(isEnvE2e && {
+      babel: {
+        plugins: ["istanbul"],
+      },
+    }),
+    jest: {
+      configure: {
+        collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+        coverageDirectory: "jest-coverage",
+      },
+    },
+  };
+};
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,13 +1,13 @@
+const codeCoveragePlugin = require("@cypress/code-coverage/task");
 const path = require("path");
 const dotenv = require("dotenv");
 
 module.exports = (on, config) => {
+  codeCoveragePlugin(on, config);
   const configWithDotenv = dotenv.config({
-    // .env.test 파일의 환경 변수 load
     path: path.resolve(process.cwd(), ".env.test"),
   });
 
-  // cypress.json 파일에서 주입 가능한 환경 변수와 .env.test 파일의 환경 변수를 합침
   const env = { ...config.env, ...configWithDotenv.parsed };
   const result = { ...config, env };
 
